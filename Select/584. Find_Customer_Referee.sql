0cc7048a40709d9ddbcb4dcaecf43589c0358a4c
gowofmath-art <gowofmath@gmail.com>
2026-08-17 01:34:03 +0800

    584

--- Select/584. Find_Customer_Referee.sql
+++ Select/584. Find_Customer_Referee.sql
@@ -9,3 +9,9 @@
 -- 由任何客戶推薦，通過 id != 2
 -- 未曾由任何客戶推薦。
 
+-- 集合是整個 Customer ，找的對象是 name，接著就是條件(不要 =2 以及有缺值的 )
+-- 缺值要用 is null
+
+Select name 
+from Customer 
+where referee_id != 2 or referee_id is null;
