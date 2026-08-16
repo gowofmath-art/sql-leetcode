c909198eab4f23a8adaef7ce8e27260906f6daeb
gowofmath-art <gowofmath@gmail.com>
2026-08-17 01:14:51 +0800

    1757

---
+++ Select/1757. Recyclable_and_Low_Fat_Products.sql
@@ -0,0 +1,12 @@
+-- LeetCode 1757
+-- Recyclable and Low Fat Products
+
+-- Write a solution to find the ids of products that are both low fat and recyclable.
+-- 編寫一個解決方案，找出同時屬於“低脂”和“可回收”的產品 ID。
+
+Select product_id 
+from Products 
+where low_fats = 'Y' and recyclable = 'Y'
+
+
+
